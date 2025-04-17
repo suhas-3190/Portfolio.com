b27707ebc54bcabe2507acf764938166f1e52c7c
Kethan3 <kethangowda003@gmail.com>
2025-04-17 09:27:56 +0530

    update layout and page structure, and add NavigationBar component

---
+++ app/components/navigation-bar/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavigationBar = (props :{className? : string}) => {
+  return (
+    <>
+    <div></div>
+    </>
+  )
+}
+
+export default NavigationBar
